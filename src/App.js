80aaf142caed6f61185ead4c6933f9a18213c59b
Matt Hagner <web@matthagner.com>
2019-06-05 12:59:09 -0500

    Add Search toggle and clear features

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import TodoList from "./components/TodoComponents/TodoList";
 import TodoForm from "./components/TodoComponents/TodoForm";
+import SearchForm from "./components/SearchComponents/Form";
 
 import "./App.css";
 
@@ -9,8 +10,28 @@ class App extends React.Component {
     super(props);
 
     this.state = {
-      todos: []
+      todos: [],
+      search: ""
     };
+  }
+
+  componentDidMount() {
+    let initialTodos = window.localStorage.getItem("react-todo-todos");
+
+    if (initialTodos !== null) {
+      this.setState({
+        todos: JSON.parse(initialTodos)
+      });
+    }
+  }
+
+  componentDidUpdate(_prevProps, prevState) {
+    if (prevState.todos !== this.state.todos) {
+      window.localStorage.setItem(
+        "react-todo-todos",
+        JSON.stringify(this.state.todos)
+      );
+    }
   }
 
   addTodo = (task, id = Date.now()) => {
@@ -45,14 +66,35 @@ class App extends React.Component {
     }));
   };
 
+  filterTodos = () => {
+    const regex = new RegExp(`${this.state.search}`);
+    return this.state.todos.filter(todo => regex.test(todo.task));
+  };
+
+  handleSearchChange = event => {
+    this.setState({
+      search: event.target.value
+    });
+  };
+
+  handleSearchClear = () => this.setState({ search: "" });
+
   render() {
+    const filteredTodos =
+      this.state.search !== "" ? this.filterTodos() : this.state.todos;
+
     return (
       <div className="app-container">
+        <SearchForm
+          searchValue={this.state.search}
+          handleChange={this.handleSearchChange}
+          handleClear={this.handleSearchClear}
+        />
         <TodoForm
           handleSubmit={this.addTodo}
           clearCompleted={this.clearCompleted}
         />
-        <TodoList tasks={this.state.todos} toggleTodo={this.toggleTodo} />
+        <TodoList tasks={filteredTodos} toggleTodo={this.toggleTodo} />
       </div>
     );
   }
--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,16 @@ import { render, fireEvent, cleanup } from "@testing-library/react";
 
 import App from "./App";
 
-afterEach(cleanup);
+afterEach(() => {
+  localStorage.clear();
+  cleanup();
+});
+
+const addTodos = (input, submitBtn, arrayOfNewTodoValues) =>
+  arrayOfNewTodoValues.forEach(value => {
+    fireEvent.change(input, { target: { value } });
+    fireEvent.click(submitBtn);
+  });
 
 test("users should be able to enter new todos", () => {
   const { getByLabelText, getByText } = render(<App />);
@@ -41,21 +50,51 @@ test("user should be able to clear completed todos", () => {
   const { getByLabelText, getByText, queryByText } = render(<App />);
 
   const input = getByLabelText("New todo:");
+  const submitBtn = getByText("Add Todo");
 
-  fireEvent.change(input, { target: { value: "task 1" } });
-  fireEvent.click(getByText("Add Todo"));
-
-  fireEvent.change(input, { target: { value: "task 2" } });
-  fireEvent.click(getByText("Add Todo"));
-
-  fireEvent.change(input, { target: { value: "task 2" } });
-  fireEvent.click(getByText("Add Todo"));
+  addTodos(input, submitBtn, ["task 1", "task 2", "task 3"]);
 
   const taskOne = getByText("task 1");
   fireEvent.click(taskOne);
+  const taskThree = getByText("task 3");
+  fireEvent.click(taskThree);
 
   const clearCompletedBtn = getByText("Clear Completed");
   fireEvent.click(clearCompletedBtn);
 
   expect(queryByText("task 1")).toEqual(null);
+  expect(queryByText("task 3")).toEqual(null);
+  expect(queryByText("task 2")).toBeTruthy();
+});
+
+test("should persist data", () => {
+  const tasks = ["task 1", "task 2", "task 3"];
+  const firstRender = render(<App />);
+
+  const todoInput = firstRender.getByLabelText("New todo:");
+  const submitBtn = firstRender.getByText("Add Todo");
+
+  addTodos(todoInput, submitBtn, tasks);
+
+  firstRender.unmount();
+
+  const secondRender = render(<App />);
+
+  tasks.forEach(task => {
+    expect(secondRender.getByText(task)).toBeTruthy();
+  });
+});
+
+test("user should be able to filter todos", () => {
+  const container = render(<App />);
+
+  const todoInput = container.getByLabelText("New todo:");
+  const submitBtn = container.getByText("Add Todo");
+  const searchInput = container.getByLabelText("Search Todos:");
+
+  addTodos(todoInput, submitBtn, ["task 1", "task 2", "also task 2"]);
+  fireEvent.change(searchInput, { target: { value: "task 2" } });
+
+  expect(container.getAllByText(/task 2/g).length).toEqual(2);
+  expect(container.queryByText("task 1")).toEqual(null);
 });
