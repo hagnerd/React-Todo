3cb006362bd8c11a2ad04c21ec3a8f71e23a8ec1
Matt Hagner <web@matthagner.com>
2019-06-04 19:43:37 -0500

    Add TodoList component

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,18 @@
-// your components will all go in this `component` directory.
-// feel free to change this component.js into TodoList.js
+import React from "react";
+import PropTypes from "prop-types";
+
+import Todo from "./Todo";
+
+export default function TodoList({ tasks }) {
+  return (
+    <ul>
+      {tasks.map(task => (
+        <Todo key={task.id} {...task} />
+      ))}
+    </ul>
+  );
+}
+
+TodoList.propTypes = {
+  tasks: PropTypes.arrayOf(PropTypes.shape(Todo.propTypes)).isRequired
+};
---
+++ src/components/TodoComponents/TodoList.test.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import TodoList from "./TodoList";
+
+const propFac = props => ({
+  tasks: [
+    { task: "mow the lawn", id: 1, completed: false },
+    { task: "cook dinner", id: 2, completed: false },
+    { task: "take down the trash", id: 3, completed: false }
+  ],
+  ...props
+});
+
+const defaultProps = propFac();
+
+test("it should render a list of items passed in", () => {
+  const { getByText } = render(<TodoList {...defaultProps} />);
+
+  defaultProps.tasks.forEach(todo => expect(getByText(todo.task)).toBeTruthy());
+});
